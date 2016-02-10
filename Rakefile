3bb69f193762b482450bc43eba09806aced1de64
zoeackerman <zoe@ztoasafetyservices.com>
2016-02-10 01:10:41 +0000

    Setup and Configuration

---
+++ Rakefile
@@ -0,0 +1,3 @@
+
+require './config/application'
+require 'sinatra/activerecord/rake'
---
+++ config/application.rb
@@ -0,0 +1,10 @@
+# require gems and classes
+require 'bundler'
+Bundler.require
+$: << File.expand_path('../', __FILE__)
+Dir['./app/**/*.rb'].sort.each { |file| require file }
+
+# configure sinatra
+set :root, Dir['./app']
+set :public_folder, Proc.new { File.join(root, 'assets') }
+set :erb, :layout => :'layouts/application'
